b848296117fb5a450c65fb4d2974b402734e72af
zhuyuzhu <struggle_zhu@163.com>
2021-06-30 20:42:57 +0800

    docs 类、接口、抽象类

--- src/index.ts
+++ src/index.ts
@@ -70,3 +70,55 @@ interface Cache {
 let cache: Cache = {} //动态添加属性和值
 cache.foo = 'foo1'
 cache.bar = 'bar1'
+
+interface EatAndRun {
+    eat (food: string): void;
+    run (distance: number): void;
+}
+interface Say {
+    say(language: string): void
+}
+
+class Person implements EatAndRun, Say{
+    constructor() {
+        
+    }
+    eat(food: string): void {
+
+    }
+    run(distance: number){
+
+    }
+    say(language: string) {
+
+    }
+}
+
+abstract class Animal {
+    eat (food: string): void {
+        console.log(`Eating ${food}`)
+    }
+    abstract run (distance: number): void; //抽象方法只声明，在子类中创建
+}
+
+class Dog extends Animal {
+    // constructor() {
+    //     super()
+    // }
+    run (distance: number): void {
+        console.log(distance)
+    }
+}
+let dog = new Dog()
+dog.run(25)
+
+function createArr<T> (length: number, value: T): T[] {
+    let arr = Array<T>(length).fill(value); //T 为类型变量，可以传number、string等
+    return arr;
+}
+let strArr = createArr<string> (3, 'abc')
+console.log(strArr) //[ 'abc', 'abc', 'abc' ]
+
+// import { camelCase } from 'lodash'
+// declare function camelCase (input: string): string; //需要手动去申明
+// let res = camelCase('hello lodash')
